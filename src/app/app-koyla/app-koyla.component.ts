8f76d659099a69730edfb1231b4256c79a3fdd59
armikhalev <armikhalev@gmail.com>
2017-03-16 19:02:34 -0400

    Adds koyla service to get json from api.

--- src/app/app-koyla/app-koyla.component.ts
+++ src/app/app-koyla/app-koyla.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AppKoylaService } from '../services/app-koyla.service'
 
 @Component({
   selector: 'app-app-koyla',
   templateUrl: './app-koyla.component.html',
   styleUrls: ['./app-koyla.component.scss']
 })
+
 export class AppKoylaComponent implements OnInit {
 
-  constructor() { }
+  private word;
+  private errorMessage;
+  private langTranslateFrom;
+  private langTranslateTo;
+
+  constructor(private koylaService: AppKoylaService) { }
 
   ngOnInit() {
+    this.word = 'Translate';
+    this.langTranslateFrom = 'english';
+    this.langTranslateTo = 'mela'
+  }
+
+  changeLanguage_onClick() {
+      this.langTranslateFrom = this.langTranslateFrom === "english" ? "mela" : "english";
+      this.langTranslateTo = this.langTranslateFrom === "english" ? "mela" : "english";
+  }
+  
+  getWord(lang:string, toTranslate:string) {
+    this.koylaService.getWord(lang, toTranslate)
+                     .subscribe(
+                       translation => this.word = translation,
+                       error =>  this.errorMessage = <any>error);
   }
 
 }
--- src/app/services/app-koyla.service.ts
+++ src/app/services/app-koyla.service.ts
@@ -7,29 +7,48 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AppKoylaService {
 
-  // private wordsUrl = 'app/heroes';  // URL to web API
-  // constructor (private http: Http) {}
-  // getHeroes (): Observable<Hero[]> {
-  //   return this.http.get(this.heroesUrl)
-  //                   .map(this.extractData)
-  //                   .catch(this.handleError);
-  // }
-  // private extractData(res: Response) {
-  //   let body = res.json();
-  //   return body.data || { };
-  // }
-  // private handleError (error: Response | any) {
-  //   // In a real world app, you might use a remote logging infrastructure
-  //   let errMsg: string;
-  //   if (error instanceof Response) {
-  //     const body = error.json() || '';
-  //     const err = body.error || JSON.stringify(body);
-  //     errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-  //   } else {
-  //     errMsg = error.message ? error.message : error.toString();
-  //   }
-  //   console.error(errMsg);
-  //   return Observable.throw(errMsg);
-  // }
+  private wordsUrl = 'http://0.0.0.0:8000/koyla/';  // URL to web API
+
+  constructor (private http: Http) {}
+
+  getWord (lang, word): Observable<any> {
+    return this.http.get(this.wordsUrl + lang + '/' + word)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body[0].la || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+
+    console.error(errMsg);
+
+    return Observable.throw(errMsg);
+  }
 
 }
+
+
+  // function submitWord(lang,word){
+
+  //    $.getJSON("http://0.0.0.0:8000/koyla/" + lang + "/" + word)
+  //    .done(function(data) {
+  //         translation = data;
+  //         submitWordEvent.notify([translation,lang]);
+  //    })
+  //    .fail(function() {
+  //        console.log('Oh no, something went wrong!');
+  //    });
+  // };
